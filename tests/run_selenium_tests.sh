c961ecf70d217d7fa2fd769e56093ad357d2a5b6
Paul Ebreo <pebreo@imagescape.com>
2016-02-02 05:06:23 -0600

    remove virtualenv requirement

--- tests/run_selenium_tests.sh
+++ tests/run_selenium_tests.sh
@@ -1,6 +1,6 @@
-trap 'rm -rf PBTESTENV' EXIT  # Clean virtualenv dir on exit
-virtualenv PBTESTENV
-source PBTESTENV/bin/activate
+#trap 'rm -rf PBTESTENV' EXIT  # Clean virtualenv dir on exit
+#virtualenv PBTESTENV
+#source PBTESTENV/bin/activate
 pip install -r sel_requirements.txt
 
 export ENABLE_XVFB=1    # run the tests headless using Xvfb. Set 0 to disable

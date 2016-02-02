167ae8d3ae1bdd652181154741998a0b51e3a572
Paul Ebreo <pebreo@imagescape.com>
2016-02-02 05:19:39 -0600

    add closerlook test cases

--- tests/selenium/basic_test.py
+++ tests/selenium/basic_test.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-class Test(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
 
     def setUp(self):
         self.xvfb = os.environ.get("ENABLE_XVFB", False)
@@ -40,9 +40,49 @@ class Test(unittest.TestCase):
                                      ["ignore-certificate-errors"])
         return webdriver.Chrome(chrome_options=opts)
 
-    def test_should_load_wikipedia(self):
-        self.load("https://www.wikipedia.org")
-        self.assertIn("Wikipedia", self.driver.title)
+    # def test_should_load_wikipedia(self):
+    #     self.load("https://www.wikipedia.org")
+    #     self.assertIn("Wikipedia", self.driver.title)
+
+class TestCloserlook(BaseTestCase):
+    
+    #@unittest.skip('wip')
+    def test_searchterm_not_found(self):
+        """ Expect no search results when given a nonsense search term """
+        self.driver.get('http://closerlook.com')
+        self.driver.find_element_by_id('menu-search').click()
+        self.driver.find_element_by_id('username').send_keys('thissearchtermwillnotbefound')
+        self.driver.save_screenshot('noresultsfound_result.png')
+        assert 'No results were found.' in self.driver.page_source
+
+
+    def test_blank_searchterm(self):
+        """ Expect redirect to mainpaige when given a blank search term """
+        self.driver.get('http://closerlook.com')
+        self.driver.find_element_by_id('menu-search').click()
+        self.driver.find_element_by_id('username').send_keys('')
+        self.driver.save_screenshot('redirect_result.png')
+        assert 'Required' in self.driver.page_source
+
+
+    def test_that_homepage_links_work(self):
+        """
+        All the homepage links should work.
+        For more comprehensive test goto: http://validator.w3.org/checklink
+        """
+        self.driver.get('http://www.closerlook.com')
+        links = self.driver.find_elements_by_xpath('//body//a[string-length(@href)>1]')
+        
+        # Filter only valid links
+        links = [l for l in links if l.get_attribute('href').startswith('http://www.closerlook')]
+        
+        for link in links:
+            href = link.get_attribute('href')
+            print "Checking link %s" % href 
+            # Use requests to grab headers of the links
+            r = requests.get(href)
+            assert r.headers
+            assert r.status_code == 200
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
